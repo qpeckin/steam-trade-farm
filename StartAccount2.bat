395574865da6ad11e692ec56eb33700d87b91062
qpeckin <64363113+qpeckin@users.noreply.github.com>
2021-02-11 15:10:13 +0100

    Update StartAccount2.bat

--- StartAccount2.bat
+++ StartAccount2.bat
@@ -2,7 +2,8 @@
 :main
 color d
 echo Script Started
-echo Github -
+echo Github -  https://github.com/qpeckin/steam-trade-farm
+echo Steam Group - https://steamcommunity.com/groups/qpeckin
 echo.                                                                                                                            
 title Account #2 - Trade Boost 
 node account2.js
