bb582c4aad8b7408e37f86b5c51fb03848fbc9a1
qpeckin <64363113+qpeckin@users.noreply.github.com>
2021-02-11 20:59:39 +0100

    Update account2.js

--- account2.js
+++ account2.js
@@ -36,7 +36,7 @@ client.on('webSession', (sessionid, cookies) => {
 
 	console.log('[-]Returning items...');
 	sendRandomItem();
-}, 240 * 1000); // 240 * 1000 ms = 4 min
+}, 240 * 1000); // 240 * 1000 ms = 4 min [replace '240' by '140' if Account2 take too much time to send the inventory back]
 });
 
 manager.on('newOffer', (offer) => {
